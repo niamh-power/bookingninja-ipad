98f38fcd0e6c78c075ccfbbbd43b589536edd841
Niamh Power <niamhpower@live.com>
2022-01-11 14:01:55 +0000

    Set up project

---
+++ Booking Ninja/Shared/AgendaScreen.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct AgendaScreen: View {
+    let backgroundColor = UIColor(named: "backgroundColor")
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Text("Testing")
+        }
+    }
+}
---
+++ Booking Ninja/Shared/Booking_NinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Booking_NinjaApp.swift
+//  Shared
+//
+//  Created by Niamh Power on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Booking_NinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AgendaScreen()
+        }
+    }
+}
